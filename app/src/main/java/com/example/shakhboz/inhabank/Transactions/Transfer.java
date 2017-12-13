2f1ab7b79286b9bf41ec3d5a3613b6fb480aa646
fredrep90 <fredrep90@gmail.com>
2017-12-13 20:17:51 +0900

    Transfer changed

--- app/src/main/java/com/example/shakhboz/inhabank/Transactions/Transfer.java
+++ app/src/main/java/com/example/shakhboz/inhabank/Transactions/Transfer.java
@@ -2,9 +2,14 @@ package com.example.shakhboz.inhabank.Transactions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.shakhboz.inhabank.R;
+
+import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,10 +18,24 @@ public class Transfer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button submit=findViewById(R.id.submit_btn);
+        TextView tr_date=findViewById(R.id.transfer_date);
+        EditText amount=findViewById(R.id.transfer_amount);
+        EditText transfering_user=findViewById(R.id.transfer_to_user);
         setContentView(R.layout.activity_transfer);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd   HH:mm:ss");
         String currentDateTime = sdf.format(new Date());
-        TextView tr_date=findViewById(R.id.transfer_date);
         tr_date.setText(currentDateTime);
+
+        submit.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
     }
+
+
 }

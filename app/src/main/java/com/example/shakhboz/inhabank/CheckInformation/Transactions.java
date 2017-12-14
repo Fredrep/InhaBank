68703169b1fb1468d7af755fb046903df82e18d8
Nuriddinbek <kodirovn95@gmail.com>
2017-12-13 16:49:50 -0800

    Deposit updated

--- app/src/main/java/com/example/shakhboz/inhabank/CheckInformation/Transactions.java
+++ app/src/main/java/com/example/shakhboz/inhabank/CheckInformation/Transactions.java
@@ -1,9 +1,15 @@
 package com.example.shakhboz.inhabank.CheckInformation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.shakhboz.inhabank.R;
+import com.example.shakhboz.inhabank.Transactions.Deposit;
+import com.example.shakhboz.inhabank.Transactions.Transfer;
+import com.example.shakhboz.inhabank.Transactions.Withdraw;
 
 public class Transactions extends AppCompatActivity {
 
@@ -12,6 +18,39 @@ public class Transactions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
 
+        Button toDepositButton;
+        Button toWithdrawButton;
+        Button toTransferButton;
+
+
+        toDepositButton = (Button)findViewById(R.id.deposit_id);
+        toWithdrawButton = (Button)findViewById(R.id.withdraw_id);
+        toTransferButton = (Button)findViewById(R.id.transfer_id);
+
+
+        toDepositButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent depositIntent = new Intent(Transactions.this, Deposit.class);
+                startActivity(depositIntent);
+            }
+        });
+
+        toWithdrawButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent withdrawIntent = new Intent(Transactions.this, Withdraw.class);
+                startActivity(withdrawIntent);
+            }
+        });
+
+        toTransferButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent transferIntent = new Intent(Transactions.this, Transfer.class);
+                startActivity(transferIntent);
+            }
+        });
 
     }
 }
